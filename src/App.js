d5d963b88bef2f96913376a095ef4b64a0200e63
Chase DeAnda <chasedeanda@gmail.com>
2022-05-26 16:40:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-import "./App.css";
 function App() {
-  let arr = [];
-  const [breeds, setBreeds] = useState({});
+  const [breeds, setBreeds] = useState([]);
+
+  const formatBreedList = (data) => {
+    return Object.keys(data).reduce((_breeds, breed) => {
+      const subBreeds = data[breed];
+      if (subBreeds.length) {
+        // Breed has subbreeds
+        // iterate over each subbreed and
+        // push as new item in list
+        subBreeds.forEach((subBreed) => {
+          _breeds.push(`${subBreed} ${breed}`);
+        });
+      } else {
+        // No subbreeds, push breed name to list
+        _breeds.push(breed);
+      }
+      return _breeds;
+    }, []);
+  };
 
   useEffect(() => {
     const getData = async () => {
       let result = await axios.get("https://dog.ceo/api/breeds/list/all");
-      setBreeds(result.data.message);
+      setBreeds(formatBreedList(result.data.message));
     };
     getData();
   }, []);
-  useEffect(() => {
-    let temp = breeds;
-    console.log("temp breeds: ", breeds);
-    Object.keys(breeds).map((key, index) => {
-      console.log("breeds[key]: ", breeds[key]);
-
-      arr.push(breeds[key]);
-    });
-    console.log(arr);
-  }, [breeds]);
 
   return (
     <div className="App">
       <h1>SERA TEST</h1>
-      {Object.keys(breeds).map((key, index) => {
-        return (
-          <div key={index}>
-            {key}: {`${breeds[key]}`}
-          </div>
-        );
-      })}
+      {breeds.map((breed) => (
+        <div key={breed}>{breed}</div>
+      ))}
     </div>
   );
 }
